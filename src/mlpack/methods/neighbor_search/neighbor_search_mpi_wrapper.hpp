7df47996ddb0d7cf02c5b1ede7be7b95acf45155
ryan <ryan@ratml.org>
2015-07-21 11:26:49 -0400

    More refactoring.

--- src/mlpack/methods/neighbor_search/neighbor_search_mpi_wrapper.hpp
+++ src/mlpack/methods/neighbor_search/neighbor_search_mpi_wrapper.hpp
@@ -14,176 +14,53 @@
 namespace mlpack {
 namespace neighbor {
 
+namespace util {
+// Utility power of four function.
+// Only useful because binary trees.
+template<size_t pow>
+constexpr size_t powfour<pow>()
+{
+  return (pow == 0) ? 1 : 2 * powfour<pow - 1>();
+}
+
+}
+
 template<typename SortPolicy, typename MetricType, typename TreeType>
 class NeighborSearchMPIWrapper
 {
  public:
-  typedef NeighborSearchRules<SortPolicy, MetricType, TreeType>
-      RuleType;
-
-  NeighborSearchMPIWrapper() :
-      referenceTree(NULL),
-      queryTree(NULL),
-      rules(NULL),
-      owner(false)
-  { }
-
-  NeighborSearchMPIWrapper(
-      TreeType* referenceTree,
-      TreeType* queryTree,
-      RuleType* rules) :
-      referenceTree(referenceTree),
-      queryTree(queryTree),
-      rules(rules),
-      neighbors(rules->Neighbors()),
-      distances(rules->Distances()),
-      owner(false)
-  {
-    // Nothing left to do.
-  }
-
-  ~NeighborSearchMPIWrapper()
-  {
-    // Only delete the objects if we own them.
-    if (owner)
-    {
-      if (rules)
-        delete rules;
-      if (referenceTree)
-        delete referenceTree;
-      if (queryTree)
-        delete queryTree;
-    }
-  }
-
-  template<typename Archive>
-  void serialize(Archive& ar, const unsigned int version)
-  {
-    Serialize(ar, version);
-  }
-
-  template<typename Archive>
-  void Serialize(Archive& ar, const unsigned int /* version */)
-  {
-    if (Archive::is_loading::value && referenceTree && owner)
-      delete referenceTree;
-    if (Archive::is_loading::value && queryTree && owner)
-      delete queryTree;
-
-    ar & data::CreateNVP(referenceTree, "referenceTree");
-    ar & data::CreateNVP(queryTree, "queryTree");
-    ar & data::CreateNVP(neighbors, "neighbors");
-    ar & data::CreateNVP(distances, "distances");
-    ar & data::CreateNVP(metric, "metric");
-
-    if (Archive::is_loading::value)
-    {
-      if (rules && owner)
-        delete rules;
-
-      // Create the rules object and set the references correctly.
-      owner = true;
-      rules = new NeighborSearchRules<SortPolicy, MetricType, TreeType>(
-          referenceTree->Dataset(), queryTree->Dataset(), neighbors, distances,
-          metric);
-    }
-
-    ar & data::CreateNVP(*rules, "rules");
-  }
-
-  TreeType* ReferenceTree() const { return referenceTree; }
-  TreeType*& ReferenceTree() { return referenceTree; }
-
-  TreeType* QueryTree() const { return queryTree; }
-  TreeType*& QueryTree() { return queryTree; }
-
-  RuleType* Rules() const { return rules; }
-  RuleType*& Rules() { return rules; }
-
-  const arma::Mat<size_t>& Neighbors() const { return neighbors; }
-  arma::Mat<size_t>& Neighbors() { return neighbors; }
-
-  const arma::mat& Distances() const { return distances; }
-  arma::mat& Distances() { return distances; }
-
- private:
-  TreeType* referenceTree;
-  TreeType* queryTree;
-  RuleType* rules;
-  arma::Mat<size_t> neighbors;
-  arma::mat distances;
-  MetricType metric;
-  bool owner;
 };
 
+template<size_t TaskDepth = 6>
 class NeighborSearchMPIResultsWrapper
 {
- public:
-  NeighborSearchMPIResultsWrapper() { }
-
-  NeighborSearchMPIResultsWrapper(const arma::Mat<size_t>& neighbors,
-                                  const arma::mat& distances) :
-      neighbors(neighbors),
-      distances(distances)
-  {
-    // Nothing to do.
-  }
-
-  template<typename Archive>
-  void Serialize(Archive& ar, const unsigned int /* version */)
-  {
-    ar & data::CreateNVP(neighbors, "neighbors");
-    ar & data::CreateNVP(distances, "distances");
-  }
-
-  template<typename Archive>
-  void serialize(Archive& ar, const unsigned int version)
-  {
-    Serialize(ar, version);
-  }
-
-  const arma::Mat<size_t>& Neighbors() const { return neighbors; }
-  arma::Mat<size_t>& Neighbors() { return neighbors; }
-
-  const arma::mat& Distances() const { return distances; }
-  arma::mat& Distances() { return distances; }
-
-  template<typename SortPolicy, typename MetricType, typename TreeType>
-  void Merge(NeighborSearchRules<SortPolicy, MetricType, TreeType>& rules)
-  {
-    // The goal here is to merge our results into the results of the existing
-    // NeighborSearchRules.  We'll loop over every point.
-    for (size_t i = 0; i < neighbors.n_cols; ++i)
-    {
-      // Auxiliary arrays, since the results will be in the rules object.
-      arma::Col<size_t> oldNeighbors = rules.Neighbors().col(i);
-      arma::vec oldDistances = rules.Distances().col(i);
-
-      size_t oldIndex = 0; // Tracks where we are in the old results.
-      size_t index = 0; // Tracks where we are in the new results.
-
-      // Loop until the resulting vectors are merged properly.
-      while (index + oldIndex < rules.Neighbors().n_rows)
-      {
-        if (SortPolicy::IsBetter(distances(index, i), oldDistances(oldIndex)))
-        {
-          rules.Neighbors()(index + oldIndex, i) = neighbors(index, i);
-          rules.Distances()(index + oldIndex, i) = distances(index, i);
-          ++index;
-        }
-        else
-        {
-          rules.Neighbors()(index + oldIndex, i) = oldNeighbors(oldIndex);
-          rules.Distances()(index + oldIndex, i) = oldDistances(oldIndex);
-          ++oldIndex;
-        }
-      }
-    }
-  }
-
  private:
-  arma::Mat<size_t> neighbors;
-  arma::mat distances;
+  // Store a bit for each possible descendant combination.
+  // The bit will be set to 'true' if the descendant combination should be
+  // visited.
+  std::bitset<util::powfour<TaskDepth>> newTasks;
+
+ public:
+  NeighborSearchMPIResultsWrapper()
+  {
+    for (size_t i = 0; i < combinationBytes; ++i)
+      childCombinations[i] = 0; // Set all bits to 0.
+  }
+
+  size_t NumNewTasks() const
+  {
+    return newTasks.count();
+  }
+
+  template<typename TreeType>
+  AddToTaskQueue(TreeType* queryRoot,
+                 TreeType* referenceRoot,
+                 std::queue<std::pair<TreeType*, TreeType*>>& queue)
+  {
+    
+  }
+
+
 };
 
 } // namespace neighbor
