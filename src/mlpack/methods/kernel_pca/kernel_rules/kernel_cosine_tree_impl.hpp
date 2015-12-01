1a054931957b7839cb2a850f835a7e0747feadee
ryan <ryan@ratml.org>
2015-12-01 16:35:40 -0500

    Copypasta for comparison with Nystroem method.

--- src/mlpack/methods/kernel_pca/kernel_rules/kernel_cosine_tree_impl.hpp
+++ src/mlpack/methods/kernel_pca/kernel_rules/kernel_cosine_tree_impl.hpp
@@ -102,6 +102,94 @@ std::endl;
 }
 
 template<typename KernelType>
+KernelCosineTree<KernelType>::KernelCosineTree(const arma::mat& data,
+                                               KernelType& kernel,
+                                               const size_t rank) :
+    dataset(data),
+    kernel(kernel),
+    left(NULL),
+    right(NULL)
+{
+  Log::Info << "Building kernel cosine tree.\n";
+  // Pick a point randomly...
+  point = data.col(0); // (That's not very random.)
+
+  // Calculate self-kernels.
+  arma::vec norms(data.n_cols);
+  for (size_t i = 0; i < data.n_cols; ++i)
+    norms[i] = std::sqrt(kernel.Evaluate(data.col(i), data.col(i)));
+  pointNorm = norms[0];
+
+  // Let's find the per-point error.
+  double relativeError = CalculateError();
+  Log::Info << "Current relative error: " << relativeError << "." << std::endl;
+
+  // Build a priority queue of nodes to split.
+  std::priority_queue<std::pair<size_t, KernelCosineTree*>> queue;
+  queue.push(std::make_pair(data.n_cols, this));
+
+  for (size_t i = 0; i < rank; ++i)
+  {
+    Log::Info << "Current relative error: " << relativeError << "." <<
+std::endl;
+
+    // Split the points in this node into near and far, until we are below our
+    // desired relative error bound.
+    std::pair<size_t, KernelCosineTree*> frame = queue.top();
+    queue.pop();
+    size_t frameError = frame.first;
+    KernelCosineTree* node = frame.second;
+
+    // Now, calculate the angle between all points.
+    arma::vec angles(node->Dataset().n_cols - 1);
+    for (size_t i = 1; i < node->Dataset().n_cols; ++i)
+    {
+      angles[i - 1] = 1 - std::abs(kernel.Evaluate(node->Dataset().col(i),
+          node->Dataset().col(0))) / (norms[i] * norms[0]);
+    }
+    node->SplitValue() = arma::median(angles);
+    Log::Info << "Split value: " << node->SplitValue() << ".\n";
+
+    // Now, split the points into near and far.
+    size_t numNear = 0;
+    for (size_t i = 0; i < angles.n_elem; ++i)
+      if (angles[i] < node->SplitValue())
+        numNear++;
+
+    // Probably not the best way to do it.  But maybe it is?
+    arma::mat near(node->Dataset().n_rows, numNear + 1);
+    arma::mat far(node->Dataset().n_rows, angles.n_elem - numNear);
+    near.col(0) = node->Dataset().col(0);
+    size_t nearIndex = 1;
+    size_t farIndex = 0;
+    for (size_t i = 0; i < angles.n_elem; ++i)
+    {
+      if (angles[i] < node->SplitValue())
+      {
+        near.col(nearIndex) = node->Dataset().col(i + 1);
+        ++nearIndex;
+      }
+      else
+      {
+        far.col(farIndex) = node->Dataset().col(i + 1);
+        ++farIndex;
+      }
+    }
+
+    // Create left and right children.
+    node->Left() = new KernelCosineTree(std::move(near), kernel);
+    node->Right() = new KernelCosineTree(std::move(far), kernel);
+
+    // Now update the error calculation.
+    relativeError = CalculateError();
+    Log::Info << "New relative error: " << relativeError << ".\n";
+
+    queue.push(std::make_pair(node->Left()->Dataset().n_cols, node->Left()));
+    queue.push(std::make_pair(node->Right()->Dataset().n_cols, node->Right()));
+  }
+}
+
+template<typename KernelType>
 KernelCosineTree<KernelType>::KernelCosineTree(arma::mat&& data,
                                                KernelType& kernel) :
     pointNorm(std::sqrt(kernel.Evaluate(point, point))),
