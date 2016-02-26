4442baa1284d5f2caa109ad53fa57bdb71d3d8cd
Ryan Curtin <ryan@ratml.org>
2016-02-25 21:02:15 -0500

    Add an implementation for F > 1.

---
+++ src/mlpack/methods/hoeffding_trees/multiple_random_dimension_split.hpp
@@ -0,0 +1,170 @@
+/**
+ * @file multiple_random_dimension_split.hpp
+ * @author Ryan Curtin
+ *
+ * This splitting strategy selects several random dimensions.  It can be
+ * understood as the Forest-RI algorithm proposed by Breiman with F set to some
+ * value.
+ */
+#ifndef __MLPACK_METHODS_HOEFFDING_TREE_MULTIPLE_RANDOM_DIMENSION_SPLIT_HPP
+#define __MLPACK_METHODS_HOEFFDING_TREE_MULTIPLE_RANDOM_DIMENSION_SPLIT_HPP
+
+#include <mlpack/core.hpp>
+
+namespace mlpack {
+namespace tree {
+
+/**
+ * The MultipleRandomDimensionSplit class is a split selection strategy for the
+ * HoeffdingTree class that selects multiple dimensions randomly for splitting.
+ * It is based on the Forest-RI algorithm proposed by Breiman, with F set to a
+ * value greater than one:
+ *
+ * @code
+ * @article{breiman2001random,
+ *   title={Random forests},
+ *   author={Breiman, Leo},
+ *   journal={Machine learning},
+ *   volume={45},
+ *   number={1},
+ *   pages={5--32},
+ *   year={2001},
+ *   publisher={Springer}
+ * }
+ * @endcode
+ *
+ * SplitCheck() will return nonzero values (indicating that the node should be
+ * split) even if the best gain is less than zero.  If the tree is underfitting,
+ * it would probably be best to either set the epsilon value to SplitCheck() to
+ * be very low (or 0), or to set a maximum number of samples to see before
+ * splitting, then set forceSplit to true.
+ */
+template<
+    typename FitnessFunction = GiniImpurity,
+    template<typename> class NumericSplitType = HoeffdingDoubleNumericSplit,
+    template<typename> class CategoricalSplitType = HoeffdingCategoricalSplit>
+class MultipleRandomDimensionSplit
+{
+ public:
+  /**
+   * Create the MultipleRandomDimensionSplit object.  This will choose the given
+   * number of random splits and initialize them with the default parameters.
+   *
+   * If numRandomSplits is set to 0, then
+   * floor(log2(datasetInfo.Dimensionality())) will be used instead, just like
+   * in Breiman's paper.
+   *
+   * @param datasetInfo Dataset information.
+   * @param numClasses Number of classes in the dataset.
+   * @param numRandomSplits Number of random splits to use.
+   */
+  MultipleRandomDimensionSplit(const data::DatasetInfo& datasetInfo,
+                               const size_t numClasses,
+                               const size_t numRandomSplits = 0);
+
+  /**
+   * Create the MultipleRandomDimensionSplit object.  This will choose the given
+   * number of random splits and initialize them using the given split objects.
+   *
+   * If numRandomSplits is set to 0, then
+   * floor(log2(datasetInfo.Dimensionality())) will be used instead, just like
+   * in Breiman's paper.
+   *
+   * @param datasetInfo Dataset information.
+   * @param numClasses Number of classes in the dataset.
+   * @param numRandomSplits Number of random splits to use.
+   * @param categoricalSplit Example categorical split to take parameters from.
+   * @param numericSplit Example numeric split to take parameters from.
+   */
+  MultipleRandomDimensionSplit(const data::DatasetInfo& datasetInfo,
+                               const size_t numClasses,
+                               const size_t numRandomSplits,
+                               const CategoricalSplitType<FitnessFunction>&
+                                   categoricalSplit,
+                               const NumericSplitType<FitnessFunction>&
+                                   numericSplit);
+
+  /**
+   * Create the MultipleRandomDimensionSplit object.  This will choose the given
+   * number of random splits specified by the other MultipleRandomDimensionSplit
+   * object and initialize them using the split objects from the given other
+   * MultipleRandomDimensionSplit.
+   */
+  MultipleRandomDimensionSplit(const data::DatasetInfo& datasetInfo,
+                               const size_t numClasses,
+                               const MultipleRandomDimensionSplit& other);
+
+  //! Set the DatasetInfo object.
+  const data::DatasetInfo*& DatasetInfo() { return datasetInfo; }
+
+  /**
+   * Train the random splits on the given point.
+   */
+  template<typename VecType>
+  void Train(const VecType& point, const size_t label);
+
+  /**
+   * Given the data we have currently collected, determine whether or not a
+   * split should be performed.  If a split should not be performed, 0 is
+   * returned; otherwise, the number of child nodes that should be created will
+   * be returned.
+   *
+   * A split will be selected if its gain is at least epsilon greater than the
+   * second-best split.  So it is possible that a split may never happen (i.e.
+   * if two identical dimensions are selected); therefore, it may be useful to
+   * use the 'forceSplit' option to force a split after a certain number of
+   * samples have been seen.
+   *
+   * @param epsilon Amount by which the best gain must be better than the second
+   *     best gain to split.
+   * @param forceSplit If true, force a split regardless of the gain (as long as
+   *     the gain is positive).
+   * @param childCounts If the node should be split, this will be filled with
+   *     the counts of each class that belong to each child.  Each column
+   *     corresponds to a single child.
+   * @param splitDimension If the node should be split, this will be set to the
+   *     dimension on which the tree should be split.
+   * @param categoricalSplitInfo If the split is on a categorical dimension,
+   *     this will be filled with the split information.
+   * @param numericSplitInfo If the split is on a numeric dimension, this will
+   *     be filled with the split information.
+   */
+  size_t SplitCheck(
+      const double epsilon,
+      const bool forceSplit,
+      arma::Mat<size_t>& childCounts,
+      size_t& splitDimension,
+      typename CategoricalSplitType<FitnessFunction>::SplitInfo&
+          categoricalSplitInfo,
+      typename NumericSplitType<FitnessFunction>::SplitInfo& numericSplitInfo);
+
+  /**
+   * Serialize the object.  Before this is called for loading, datasetInfo must
+   * be set to the desired dataset info!
+   */
+  template<typename Archive>
+  void Serialize(Archive& ar, const unsigned int /* version */);
+
+ private:
+  //! The dataset information.  We never own this.
+  const data::DatasetInfo* datasetInfo;
+
+  //! The random dimensions we are looking at.
+  arma::Col<size_t> dimensions;
+  //! Mappings from dimensions to indices in our arrays.  This information
+  //! should be combined with datasetInfo->Type().
+  std::map<size_t, size_t> dimensionMappings;
+
+  //! The list of categorical dimensions (always has size at least 1).
+  std::vector<CategoricalSplitType<FitnessFunction>> categoricalSplits;
+  //! The list of numeric dimensions (always has size at least 1).
+  std::vector<NumericSplitType<FitnessFunction>> numericSplits;
+};
+
+} // namespace tree
+} // namespace mlpack
+
+// Include implementation.
+#include "multiple_random_dimension_split_impl.hpp"
+
+#endif
---
+++ src/mlpack/methods/hoeffding_trees/multiple_random_dimension_split_impl.hpp
@@ -0,0 +1,315 @@
+/**
+ * @file multiple_random_dimension_split_impl.hpp
+ * @author Ryan Curtin
+ *
+ * Implementation of the MultipleRandomDimensionSplit class.
+ */
+#ifndef __MLPACK_METHODS_HOEFFDING_TREE_MULTIPLE_RANDOM_DIMENSION_SPLIT_IMPL_HPP
+#define __MLPACK_METHODS_HOEFFDING_TREE_MULTIPLE_RANDOM_DIMENSION_SPLIT_IMPL_HPP
+
+// In case it hasn't been included yet.
+#include "multiple_random_dimension_split.hpp"
+
+namespace mlpack {
+namespace tree {
+
+template<
+    typename FitnessFunction,
+    template<typename> class NumericSplitType,
+    template<typename> class CategoricalSplitType
+>
+MultipleRandomDimensionSplit<
+    FitnessFunction,
+    NumericSplitType,
+    CategoricalSplitType
+>::MultipleRandomDimensionSplit(const data::DatasetInfo& info,
+                                const size_t numClasses,
+                                const size_t numRandomSplits) :
+    // Delegate to the other constructor.
+    MultipleRandomDimensionSplit(info, numClasses, numRandomSplits,
+        CategoricalSplitType<FitnessFunction>(1, 1),
+        NumericSplitType<FitnessFunction>(1)
+{
+  // Nothing else to do.
+}
+
+template<
+    typename FitnessFunction,
+    template<typename> class NumericSplitType,
+    template<typename> class CategoricalSplitType
+>
+MultipleRandomDimensionSplit<
+    FitnessFunction,
+    NumericSplitType,
+    CategoricalSplitType
+>::MultipleRandomDimensionSplit(const data::DatasetInfo& info,
+                                const size_t numClasses,
+                                const size_t numRandomDimensions,
+                                const CategoricalSplitType<FitnessFunction>&
+                                    categoricalSplit,
+                                const NumericSplitType<FitnessFunction>&
+                                    numericSplit) :
+    datasetInfo(&info)
+{
+  // Select a number of random dimensions.
+  if (numRandomSplits == 0)
+    math::RandomUniqueArray(0, info.Dimensionality(),
+        std::floor(std::log2(info.Dimensionality())), dimensions);
+  else
+    math::RandomUniqueArray(0, info.Dimensionality(), numRandomSplits,
+        dimensions);
+
+  // Now, create the necessary split objects and mappings.
+  for (size_t i = 0; i < dimensions.n_elem; ++i)
+  {
+    if (info.Type(dimensions[i]) == data::Datatype::categorical)
+    {
+      dimensionMappings[dimensions[i]] = categoricalSplits.size();
+      categoricalSplits.push_back(CategoricalSplitType<FitnessFunction>(
+          info.NumMappings(dimensions[i]), numClasses));
+    }
+    else if (info.Type(dimensions[i]) == data::Datatype::numeric)
+    {
+      dimensionMappings[dimensions[i]] = numericSplits.size();
+      numericSplits.push_back(NumericSplitType<FitnessFunction>(numClasses));
+    }
+  }
+
+  // Make sure we have at least one element in each.
+  if (numericSplits.size() == 0)
+    numericSplits.push_back(NumericSplitType<FitnessFunction>(numClasses));
+  if (categoricalSplits.size() == 0)
+    categoricalSplits.push_back(CategoricalSplitType<FitnessFunction>(1,
+        numClasses));
+}
+
+template<
+    typename FitnessFunction,
+    template<typename> class NumericSplitType,
+    template<typename> class CategoricalSplitType
+>
+MultipleRandomDimensionSplit<
+    FitnessFunction,
+    NumericSplitType,
+    CategoricalSplitType
+>::MultipleRandomDimensionSplit(const data::DatasetInfo& info,
+                                const size_t numClasses,
+                                const size_t numRandomDimensions,
+                                const MultipleRandomSplitType& other) :
+    // Delegate to the other constructor.
+    MultipleRandomSplitType(info, numClasses, numRandomSplits,
+        other.categoricalSplits[0], other.numericSplits[0])
+{
+  // Nothing else to do.
+}
+
+/**
+ * Train the random splits on the given point.
+ */
+template<
+    typename FitnessFunction,
+    template<typename> class NumericSplitType,
+    template<typename> class CategoricalSplitType
+>
+template<typename VecType>
+void MultipleRandomDimensionSplit<
+    FitnessFunction,
+    NumericSplitType,
+    CategoricalSplitType
+>::Train(const VecType& point, const size_t label)
+{
+  // Pass the point to each split.
+  for (size_t i = 0; i < dimensions.n_elem; ++i)
+  {
+    if (datasetInfo->Type(dimensions[i]) == data::Datatype::categorical)
+      categoricalSplits[dimensionMappings[dimensions[i]]].Train(point, label);
+    else if (datasetInfo->Type(dimensions[i]) == data::Datatype::numeric)
+      numericSplits[dimensionMappings[dimensions[i]]].Train(point, label);
+  }
+}
+
+//! Check to see if we should split.
+template<
+    typename FitnessFunction,
+    template<typename> class NumericSplitType,
+    template<typename> class CategoricalSplitType
+>
+size_t MultipleRandomDimensionSplit<
+    FitnessFunction,
+    NumericSplitType,
+    CategoricalSplitType
+>::SplitCheck(const double epsilon,
+              const bool forceSplit,
+              arma::Mat<size_t>& childCounts,
+              size_t& splitDimension,
+              typename CategoricalSplitType<FitnessFunction>::SplitInfo&
+                  categoricalSplitInfo,
+              typename NumericSplitType<FitnessFunction>::SplitInfo&
+                  numericSplitInfo)
+{
+  double largest = -DBL_MAX;
+  double secondLargest = -DBL_MAX;
+  size_t bestDimension;
+
+  // Loop over all dimensions and get the gain.
+  for (size_t i = 0; i < dimensions.n_elem; ++i)
+  {
+    double dimLargest, dimSecondLargest;
+    data::Datatype dimType;
+
+    if (datasetInfo->Type(dimensions[i]) == data::Datatype::categorical)
+    {
+      const size_t index = dimensionMappings[dimensions[i]];
+      categoricalSplits[index].EvaluateFitnessFunction(dimLargest,
+          dimSecondLargest);
+    }
+    else if (datasetInfo->Type(dimensions[i]) == data::Datatype::numeric)
+    {
+      const size_t index = dimensionMappings[dimensions[i]];
+      numericSplits[index].EvaluateFitnessFunction(dimLargest,
+          dimSecondLargest);
+    }
+
+    // Is it a new largest or second largest gain?
+    if (dimLargest > largest)
+    {
+      secondLargest = largest;
+
+      // We have to save the type too.
+      bestType = dimType;
+      bestDimension = i; // Unmapped.
+      largest = dimLargest;
+    }
+    else if (dimLargest > secondLargest)
+    {
+      secondLargest = dimLargest;
+    }
+
+    if (dimSecondLargest > secondLargest)
+    {
+      secondLargest = dimSecondLargest;
+    }
+  }
+
+  // Now, are these far enough to split?  Note that we don't rigorously enforce
+  // a gain greater than zero...
+  if (((largest - secondLargest) > epsilon) || forceSplit)
+  {
+    // We should split, so fill the necessary information and return the number
+    // of children.
+    size_t numChildren = 0;
+    if (datasetInfo->Type(dimensions[bestDimension]) ==
+        data::Datatype::categorical)
+    {
+      const size_t index = dimensionMappings[dimensions[i]];
+      numChildren = categoricalSplits[index].NumChildren();
+      categoricalSplits[index].Split(childCounts, categoricalSplitInfo);
+    }
+    else if (datasetInfo->Type(dimension[bestDimension]) ==
+        data::Datatype::numeric)
+    {
+      const size_t index = dimensionMappings[dimensions[i]];
+      numChildren = numericSplits[index].NumChildren();
+      numericSplits[index].Split(childCounts, numericSplitInfo);
+    }
+
+    splitDimension = dimensions[bestDimension];
+    return numChildren;
+  }
+  else
+  {
+    // Don't split.
+    return 0;
+  }
+}
+
+//! Serialize the object.
+template<
+    typename FitnessFunction,
+    template<typename> class NumericSplitType,
+    template<typename> class CategoricalSplitType
+>
+template<typename Archive>
+void MultipleRandomDimensionSplit<
+    FitnessFunction,
+    NumericSplitType,
+    CategoricalSplitType
+>::Serialize(Archive& ar, const unsigned int /* version */)
+{
+  using data::CreateNVP;
+
+  ar & CreateNVP(dimensions, "dimensions");
+  ar & CreateNVP(dimensionMappings, "dimensionMappings");
+
+  // We never own the dataset info.  We assume that the node that owns us will
+  // set the dataset info... before Serialize() is called!
+  if (Archive::is_loading::value)
+  {
+    numericSplits.clear();
+    categoricalSplits.clear();
+  }
+
+  // Figure out how many of each split type we need.  Keep in mind that we
+  // have to serialize at least one categorical and one numeric split.
+  size_t numNumeric = 0;
+  size_t numCategorical = 0;
+  bool hasExtraNumeric = false;
+  bool hasExtraCategorical = false;
+
+  for (size_t i = 0; i < datasetInfo->Dimensionality(); ++i)
+  {
+    if (datasetInfo->Type(i) == data::Datatype::categorical)
+      ++numCategorical;
+    else if (datasetInfo->Type(i) == data::Datatype::numeric)
+      ++numNumeric;
+  }
+
+  if (numCategorical == 0)
+  {
+    ++numCategorical;
+    hasExtraCategorical = true;
+  }
+  if (numNumeric == 0)
+  {
+    ++numNumeric;
+    hasExtraNumeric = true;
+  }
+
+  // Set size of arrays, if necessary.
+  if (Archive::is_loading::value)
+  {
+    numericSplits.resize(numNumeric, NumericSplitType<FitnessFunction>(1));
+    categoricalSplits.resize(numCategorical,
+        CategoricalSplitType<FitnessFunction>(1, 1));
+  }
+
+  // Now loop through and serialize each split.
+  for (size_t i = 0; i < dimensions.n_elem; ++i)
+  {
+    if (datasetInfo->Type(dimensions[i]) == data::Datatype::categorical)
+    {
+      std::ostringstream name;
+      name << "categoricalSplit" << i;
+      ar & CreateNVP(categoricalSplits[dimensionMappings[dimensions[i]]],
+          name.str());
+    }
+    else if (datasetInfo->Type(dimensions[i]) == data::Datatype::numeric)
+    {
+      std::ostringstream name;
+      name << "numericSplit" << i;
+      ar & CreateNVP(numericSplits[dimensionMappings[dimensions[i]]],
+          name.str());
+    }
+  }
+
+  // Do we need to serialize anything extra?
+  if (hasExtraCategorical)
+    ar & CreateNVP(categoricalSplits[0], "extraCategoricalSplit");
+  else if (hasExtraNumeric)
+    ar & CreateNVP(numericSplits[0], "extraNumericSplit");
+}
+
+} // namespace tree
+} // namespace mlpack
+
+#endif
